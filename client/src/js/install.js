182c3edeae9590e692f53f05f884803b5c69661d
Rob G <grahamrob88@gmail.com>
2023-08-30 18:26:03 -0700

    Udate json script

---
+++ client/src/js/install.js
@@ -0,0 +1,26 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    window.deferredPrompt = event
+
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+
+if(window.deferredPrompt) {
+return  
+}
+
+window.deferredPrompt.prompt()
+
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+
+    window.deferredPrompt = null
+});
